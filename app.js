b4b0b671a324ce894384546d07d0a191598387b9
Iago Germano <iagog2005@gmail.com>
2024-01-09 15:23:00 -0300

    remove hardcoded connection on main app

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-var models = require('./db/models');
 var user = require("./db/models").User;
 
 require('./config/passport/passport.js')(passport, user);
